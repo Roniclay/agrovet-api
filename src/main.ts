877434d16ac07899b74bb1b1ac4712a22590c15d
Roniclay Rodrigues <roniclayrodrigues07@gmail.com>
2025-12-07 11:33:04 -0300

    chore: ajuste de cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3001', 'https://agrovet-front.vercel.app/'],
+    origin: [
+      'http://localhost:3001',
+      'https://agrovet-front.vercel.app', // sem barra no final
+    ],
     credentials: true,
   });
 
